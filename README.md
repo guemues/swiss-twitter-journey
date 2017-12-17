e9a979ca31013e085c2b56a5fb0f040722129457
Orçun Gümüş <orcungumus@gmail.com>
2017-12-17 17:52:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Swiss Twitter Journey
+
+> This is an student project to analyze and visualize twitter usage of Swiss people.
+> This is a web site repository which uses Tableau in general.
+
+
+For a detailed explanation on how things work, check out our other documents
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+
+gulp.task(
+  'browserSync', function(){
+    browserSync.init(
+      {
+        server: {
+          baseDir: './'
+        }
+      }
+    )
+  }
+)
+
+gulp.task(
+  'watch', ['browserSync'], function() {
+    gulp.watch('./index.html', browserSync.reload)
+    gulp.watch('./css/*.css', browserSync.reload)
+    gulp.watch('./js/*.js', browserSync.reload)
+  }
+)
